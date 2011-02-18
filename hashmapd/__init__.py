2cd7d79f6aa46eff0356ede86e3c6e3138843cfb
Edward Abraham <edward@dragonfly.co.nz>
2011-02-18 22:22:06 +1300

    Not for Miles ... avoid from blah import * ...

--- hashmapd/__init__.py
+++ hashmapd/__init__.py
@@ -7,3 +7,9 @@ from hashmapd.querycouch import *
 #         is this the 'standard' way to do this or am I missing things?
 #        at the moment I'm working on the idea that into __init__.py its common to put imports
 #       for all the bits we expect callees to want to have 'easy' access to and require import hashmapd.xyz for more internal 'xyz' bits ? - MKT 2010--01
+
+#Hi Miles
+#No this is an unusual way of doing things :-) Generally __init__.py is and
+#empty, and you should also avoid "from module import *" as it causes
+#namespace pollution, that will inevitably lead to hard to find bugs ...
+#An opportunity here for me to refactor!
