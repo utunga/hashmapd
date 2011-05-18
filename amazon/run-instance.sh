da5c63dd18871844c6822beffa1bb7454fd2e2ec
Edward Abraham <edward@dragonfly.co.nz>
2011-05-18 16:58:41 +1200

    Made teh amazon ec2 script more useful

--- amazon/run-instance.sh
+++ amazon/run-instance.sh
@@ -1,19 +1,40 @@
 #! /bin/bash
 #Fire up an ubuntu small machine in the western US. We have an EBS in the 1a availability zone
-ec2-run-instances ami-19bfef5c --instance-type m1.small --region us-west-1 -z us-west-1a -K $EC2_DEFAULT_PEM -k $EC2_PRIVATE_KEY
+#ec2-run-instances ami-19bfef5c --instance-type m1.small --region us-west-1 -z us-west-1a -k edward-ec2-hashmapd
 
 #Wait a bit and then associate the ip address and volume
 EC2_VOLUME=`ec2-describe-volumes|awk '{print $2}'|head -1`
 EC2_INSTANCE=`ec2-describe-instances|grep running|awk '{print $2}'|head -1`
 EC2_ADDR=`ec2-describe-addresses|awk '{print $2}'|head -1`
-ec2-associate-address $EC2_ADDR -i $EC2_INSTANCE
-ec2-attach-volume $EC2_VOLUME -i $EC2_INSTANCE -d /dev/sdh
+EC2_SSH="ssh ubuntu@${EC2_ADDR} -i ${EC2_DEFAULT_PEM}"
+ec2-associate-address ${EC2_ADDR} -i ${EC2_INSTANCE}
+ec2-attach-volume ${EC2_VOLUME} -i ${EC2_INSTANCE} -d /dev/sdh
 
-#Lets head over to our new machine
-ssh ubuntu@$EC2_ADDR -i $EC2_DEFAULT_PEM
-#The first time, you need to format the ebs ... #sudo mfs.ext3 /dev/sdh
-#Now mount the ebs storage
-sudo mkdir /ebs
-sudo mount -t ext3 /dev/sdh /ebs
-sudo chown /ebs ubuntu
+
+
+#Set up block storage
+${EC2_SSH} "sudo mkdir /ebs"
+${EC2_SSH} "sudo mount -t ext3 /dev/sdh /ebs"
+${EC2_SSH} "sudo chown ubuntu:ubuntu /ebs"
+
+#Install software
+${EC2_SSH} "sudo aptitude update -y"
+${EC2_SSH} "sudo aptitude install couchdb git-core python-setuptools python-dev gcc -y"
+${EC2_SSH} "sudo easy_install pip"
+${EC2_SSH} "sudo pip install couchapp couchdb tweepy config"
+${EC2_SSH} -A "git clone git@github.com:utunga/hashmapd.git"
+
+#Set the couch config so that the database files go to /ebs and the index files go to /mnt
+scp -i ${EC2_DEFAULT_PEM} local.ini  ubuntu@${EC2_ADDR}:~
+${EC2_SSH} "sudo mv ~/local.ini /etc/couchdb/local.ini"
+${EC2_SSH} "sudo chown couchdb:couchdb /etc/couchdb/local.ini"
+${EC2_SSH} "sudo mkdir -p /ebs/var/lib/couchdb/0.10.0 /mnt/var/lib/couchdb/0.10.0 /ebs/var/log/couchdb/"
+${EC2_SSH} "sudo chown couchdb:couchdb /ebs/var/lib/couchdb/0.10.0 /mnt/var/lib/couchdb/0.10.0 /ebs/var/log/couchdb/"
+${EC2_SSH} "sudo chmod a+r /ebs/var/lib/couchdb/0.10.0 /mnt/var/lib/couchdb/0.10.0 /ebs/var/log/couchdb/"
+${EC2_SSH} "sudo /etc/init.d/couchdb stop"
+${EC2_SSH} "sudo /etc/init.d/couchdb start"
+
+#Set up an ssh tunnel, you can now see couch on 
+ssh -f -L 5994:localhost:5984 ubuntu@${EC2_ADDR} -i ${EC2_DEFAULT_PEM}
+
 
