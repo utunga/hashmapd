4f8c50f51ba6f8df8ef02d56a36f0cbd289222c9
Douglas Bagnall <douglas@paradise.net.nz>
2011-05-31 17:07:32 +1200

    html5 frontend: switch to xy coordinates, with debugging labels.

--- html5_frontend/bare-canvas.js
+++ html5_frontend/bare-canvas.js
@@ -1,4 +1,6 @@
-var PADDING = 16;
+var hm_globals = {
+    PADDING: 16
+};
 
 /** make_colour_range utility
  *
@@ -24,32 +26,51 @@ function make_colour_range(){
  */
 
 function hm_on_data(canvas, data){
+    var i;
     var ctx = canvas.getContext("2d");
     var rows = data.rows;
-    var scale = (canvas.width - 2 * PADDING) / (1 << rows[0].key.length);
+    var width = canvas.width - 2 * hm_globals.PADDING;
+    var height = canvas.height - 2 * hm_globals.PADDING;
     var max_value = 0;
-    for (var j = 0; j < rows.length; j++){
-        var r = rows[j];
-        if (r.value > max_value){
-            max_value = r.value;
+    var max_x = -1e999;
+    var max_y = -1e999;
+    var min_x =  1e999;
+    var min_y =  1e999;
+    /*find the coordinate and value ranges */
+    for (i = 0; i < rows.length; i++){
+        var r = rows[i];
+        max_value = Math.max(r.value, max_value);
+        max_x = Math.max(r.key[0], max_x);
+        max_y = Math.max(r.key[1], max_y);
+        min_x = Math.min(r.key[0], min_x);
+        min_y = Math.min(r.key[1], min_y);
+    }
+
+    ctx.fillStyle = "#770";
+    ctx.font = "10px Inconsolata";
+    var range_x = max_x - min_x;
+    var range_y = max_y - min_y;
+    var x_scale = width / range_x;
+    var y_scale = height / range_y;
+
+    var labels = [];
+    for (i = 0; i < rows.length; i++){
+        var r = rows[i];
+        var x = hm_globals.PADDING + r.key[0] * x_scale;
+        var y = hm_globals.PADDING + r.key[1] * y_scale;
+        ctx.fillRect(x, y, x_scale, y_scale);
+        if (i % 100 == 0){
+            labels.push([r, x, y]);
         }
     }
-    var colours = make_colour_range();
-    var value_scale = 1.0 / max_value;
-    var colour_scale = value_scale * (colours.length - 0.1);
 
-    for (var j = 0; j < rows.length; j++){
-        var r = rows[j];
-        var coords = r.key;
-        var x = 0;
-        var y = 0;
-        for (var i = 0; i < coords.length; i++){
-           var p = coords[i];
-            x = (x << 1) | (p & 1);
-            y = (y << 1) | (p >> 1);
-        }
-        ctx.fillStyle = colours[parseInt(r.value * colour_scale)];
-        ctx.fillRect(PADDING + x * scale, PADDING + y * scale,
-                 scale, scale);
+    for (i = 0; i < labels.length; i++){
+        var d = labels[i];
+        var key = d[0].key;
+
+        ctx.fillStyle = "#f00";
+        ctx.fillRect(d[1], d[2], x_scale, y_scale);
+        ctx.fillStyle = "#000";
+        ctx.fillText(("" + key[0]).substr(0,6) + "," + ("" + key[1]).substr(0, 6), d[1], d[2]);
     }
 }
--- website/setup/couch/user/views/locations/map.js
+++ website/setup/couch/user/views/locations/map.js
@@ -1,24 +1,5 @@
 
 map = function(doc) {
-  if (doc.doc_type != "twuser") return;
-    /* Scales:
-     *  7:  128 x 128
-     *  8:  256 x 256
-     *  9:  512 x 512
-     * 10:  1024 x 1024
-     * 11:  2048 x 2048
-     * 12:  4096 x 4096
-     * */
-
-  MAX_ZOOM = 9;
-
-  var quadKey = [];
-  var x = doc.x_coord;
-  var y = doc.y_coord;
-  for(var i = MAX_ZOOM -1; i >= 0; i--) {
-    quadKey[i] = ((x & 1) + (y & 1) * 2);
-    x >>= 1;
-    y >>= 1;
-  }
-  emit(quadKey, 1);
+    if (doc.doc_type != "twuser") return;
+    emit([doc.x_coord, doc.y_coord], 1);
 }
