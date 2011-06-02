bdb58197bef7516cb525cd4887c89e6bbb4cefcd
Douglas Bagnall <douglas@paradise.net.nz>
2011-06-02 13:33:02 +1200

    frontend_dev couch: reinstate quadtree view, this time with translation and scaling

--- website/setup/couch/user/views/locations/map.js
+++ website/setup/couch/user/views/locations/map.js
@@ -1,5 +1,14 @@
-
 map = function(doc) {
-    if (doc.doc_type != "twuser") return;
-    emit([doc.x_coord, doc.y_coord], 1);
+    /* NB the comment starting !code below is *not just a comment*
+       it expands into the entirety of 'quadtree.js' because its a couchapp 'directive'
+       see http://wiki.apache.org/couchdb/JavascriptPatternViewCommonJs
+    */
+    // !code lib/hashmapd_quadtree.js
+
+    if (doc.doc_type == "twuser"){
+        var coords = convert_coords(doc.x_coord, doc.y_coord);
+        if (coords !== undefined){
+            emit(coords, 1);
+        }
+    }
 }
