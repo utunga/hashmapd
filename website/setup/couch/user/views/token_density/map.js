e9ecf0f0ae8bb8408af31f07f897087b75ec0b12
Douglas Bagnall <douglas@paradise.net.nz>
2011-06-03 15:08:45 +1200

    couch views: add token_density view
    This emits the token as part of the key, before the quadtree
    coordinates, and the user_wordcount as the value.
    
    This reduces and groups properly with a summing reduce step, so long
    as you remember to add one to the group_level to account for the
    token.

---
+++ website/setup/couch/user/views/token_density/map.js
@@ -0,0 +1,19 @@
+function(doc) {
+    /* NB the comment starting !code below is *not just a comment* it
+       expands into the entirety of 'hashmapd_quadtree.js' because it
+       is a couchapp 'directive' see
+       http://wiki.apache.org/couchdb/JavascriptPatternViewCommonJs
+    */
+    // !code lib/hashmapd_quadtree.js
+
+    /* The value is a [token, wordcount] pair, can be reduced with
+     * nearby tokens by summing the word counts.
+     */
+    if (doc.doc_type == "token_n_coords"){
+        var coords = convert_coords(doc.x_coord, doc.y_coord);
+        if (coords !== undefined){
+            var key = [doc.token].concat(coords);
+            emit(key, doc.user_wordcount);
+        }
+    }
+}
