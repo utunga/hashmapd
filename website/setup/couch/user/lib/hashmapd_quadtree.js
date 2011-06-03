a81356eff05438a57bad356c05cc8b2beceefcad
Douglas Bagnall <douglas@paradise.net.nz>
2011-06-04 09:48:46 +1200

    couch quadtree view, nuked trailing whitespace

--- website/setup/couch/user/lib/hashmapd_quadtree.js
+++ website/setup/couch/user/lib/hashmapd_quadtree.js
@@ -4,7 +4,7 @@
  * It is important for the coordinates to all be POSITIVE integers and
  * for their upper bound to be known POWER OF 2.  Otherwise you get
  * rubbish.  That is the reason for all this scaling, translation, and
- * winnowing. 
+ * winnowing.
  */
 
 /* NOTE: this code gets included *within a function body*, but that
@@ -22,7 +22,7 @@ var Y_FLIP = true;
  */
 var X_TRANSLATE = 128;
 var Y_TRANSLATE = 128;
-    
+
 /*bounds checking is done BEFORE translation.  The lower bounds can't
  * be smaller than the coreesponding translation, or negative numbers
  * might sneak through.
@@ -35,12 +35,12 @@ var Y_BOUND_LOW = -Y_TRANSLATE;
 var Y_BOUND_HIGH = Y_TRANSLATE;
 
 /*what is the ultimate scale (in bits) / number of coordinates
- * 12 ->  4k square 
+ * 12 ->  4k square
  * 13 ->  8k   "
  * 14 -> 16k   "
  * 15 -> 32k   "
  * 16 -> 64k   "
- * 
+ *
  * group_level can't go deeper than 12, so you can get 1-12, or all
  * coordinates (using group=exact).
  */
@@ -52,9 +52,9 @@ function convert_coords(x, y){
     if (Y_FLIP) {
         y = -y;
     }
-    if (x <= X_BOUND_LOW || 
+    if (x <= X_BOUND_LOW ||
         x >= X_BOUND_HIGH ||
-        y <= Y_BOUND_LOW || 
+        y <= Y_BOUND_LOW ||
         y >= Y_BOUND_HIGH) {
         return undefined;
     }
@@ -62,9 +62,9 @@ function convert_coords(x, y){
     y += Y_TRANSLATE;
     x = parseInt(x * X_SCALE);
     y = parseInt(y * Y_SCALE);
-    var quadkey = [];    
+    var quadkey = [];
     for (var i = MAX_QUAD_COORDS; i > 0; i--) {
-        quadkey[i - 1] = (2 * (y & 1) + (x & 1)); 
+        quadkey[i - 1] = (2 * (y & 1) + (x & 1));
         x >>= 1;
         y >>= 1;
     }
