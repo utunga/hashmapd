73d3c75097d8c5ecedf7a51d183aa851d8d8adb7
Peter Maxwell <pm67nz@gmail.com>
2011-06-17 13:52:32 +1200

    Print date and last git commit at start of output

--- projects/fake_txt/run_synth_to_end.sh
+++ projects/fake_txt/run_synth_to_end.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+set -e
+date
+echo
+git --no-pager log -1 -b
+echo
 python generate_synthetic_raw_data.py -f synth_config
-arch sh ./prepare_train_map.sh -f synth_config
+sh ./prepare_train_map.sh -f synth_config
 
