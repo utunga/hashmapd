7cacd71abeb07843a6163a4d16293242133435ca
Peter Maxwell <pm67nz@gmail.com>
2011-06-17 15:58:18 +1200

    Tidy outout of run_synth_to_end.sh

--- projects/fake_txt/run_synth_to_end.sh
+++ projects/fake_txt/run_synth_to_end.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
+echo "Output of" $0 $@
+echo
 date
 echo
-git --no-pager log -1 -b
+git --no-pager log -1 | head -1
 echo
 python generate_synthetic_raw_data.py -f synth_config
 sh ./prepare_train_map.sh -f synth_config
