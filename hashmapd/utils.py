afa657b8bdc35923873e6cb18b746dc96ad32a4d
Peter Maxwell <pm67nz@gmail.com>
2011-06-27 00:43:43 +1200

    Rows/columns were confused at one point in trace image layout

--- hashmapd/utils.py
+++ hashmapd/utils.py
@@ -36,13 +36,13 @@ def tiled_array_image(A):
         (tiles, pixels) = (pixels, tiles)
     x = int(numpy.sqrt(pixels))
     y = (pixels-1) // x + 1
-    img_shape = (x, y)
+    img_shape = (y, x)
     pad = 1 if max(x,y) < 4 else 2
     tile_spacing = (pad, pad)
-    (x1, y1) = [d+s for (d,s) in zip(img_shape, tile_spacing)]
+    (y1, x1) = [d+s for (d,s) in zip(img_shape, tile_spacing)]
     X = min((300 // x1) + 1, int(numpy.sqrt(tiles))+1)
     Y = min((300 // y1) + 1, (tiles-1) // X + 1)
-    tile_shape = (X, Y)
+    tile_shape = (Y, X)
 
     # scale_rows_to_unit_interval
     A = A.copy()
@@ -56,13 +56,13 @@ def tiled_array_image(A):
     A = padded
 
     (xp, yp) = x+pad, y+pad
-    out_array = numpy.zeros([X*xp-pad, Y*yp-pad], dtype=A.dtype)
+    out_array = numpy.zeros([Y*yp-pad, X*xp-pad], dtype=A.dtype)
     out_array[:] = numpy.NaN
 
     for tile in range(min(X*Y, tiles)):
         (row, col) = divmod(tile, X)
-        out_array[row*xp:row*xp+x, col*yp:col*yp+y] = A[tile].reshape(img_shape)
-
+        out_array[row*yp:row*yp+y, col*xp:col*xp+x] = A[tile].reshape(img_shape)
+    
     image = PIL.Image.fromarray(255*numpy.nan_to_num(out_array)).convert("L")
     mask = PIL.Image.fromarray(255-255*(numpy.isnan(out_array))).convert("L")
     image.putalpha(mask)
