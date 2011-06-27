b1f759f543130c5f822979372d3942a955511bcb
Peter Maxwell <pm67nz@gmail.com>
2011-06-27 13:56:37 +1200

    Include parameters in train.py stdout

--- projects/mnist/train.py
+++ projects/mnist/train.py
@@ -23,6 +23,10 @@ if __name__ == '__main__':
         help="Path of the config file to use")
     (options, args) = parser.parse_args()
     cfg = LoadConfig(options.config)
+    print "PARAMETERS"
+    for section in [cfg.shape, cfg.train]:
+        for (key, value) in section.iteritems():
+            print '  {0} = {1}'.format(key, value)
     smh = train_SMH('data',
             mid_layer_sizes = list(cfg.shape.mid_layer_sizes), 
             inner_code_length = cfg.shape.inner_code_length, 
