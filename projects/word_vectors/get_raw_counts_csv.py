73824e0ee14435562ebd7bdcb211ecd70212f982
Miles Thompson <utunga@gmail.com>
2011-08-02 00:50:02 +1200

    do'h bugfix

--- projects/word_vectors/get_raw_counts_csv.py
+++ projects/word_vectors/get_raw_counts_csv.py
@@ -26,22 +26,22 @@ DEFAULT_PATH = "projects/word_vectors/tweets/"
 TWEETS_DIR = os.path.join(BASE_DIR, DEFAULT_PATH)
 ALL_TOKEN_COUNTS_FILE = 'all_token_counts.csv'
 ALL_USER_TOTALS_FILE = 'all_user_totals.csv'
-ALL_USER_TOKENS_FILE = 'all_user_token_counts.csv'
+ALL_USER_TOKEN_COUNTS_FILE = 'all_user_token_counts.csv'
 
 def write_token_counts(infile, username, token_user_counts):
     for token, count in sorted(token_user_counts.iteritems(), key=itemgetter(1), reverse=True):
         infile.write(unicode(username +","+ token +","+ str(count) +"\r\n"))
 
 def write_user_totals(user_totals):
-    debug('writing user totals to csv')
-    user_totals_file = codecs.open(USER_TOTALS_FILE, encoding="utf-8",mode= "w")
+    debug('writing ' + ALL_USER_TOTALS_FILE)
+    user_totals_file = codecs.open(ALL_USER_TOTALS_FILE, encoding="utf-8",mode= "w")
     for username, count in sorted(user_totals.iteritems(), key=itemgetter(1), reverse=True):
         user_totals_file.write(unicode(username +","+ str(count) +"\r\n"))
     user_totals_file.close()
 
 def write_token_totals(token_totals):
-    debug('writing overall token counts to csv')
-    token_counts_file = codecs.open(TOKEN_COUNTS_FILE, encoding="utf-8",mode= "w")
+    debug('writing ' + ALL_TOKEN_COUNTS_FILE)
+    token_counts_file = codecs.open(ALL_TOKEN_COUNTS_FILE, encoding="utf-8",mode= "w")
     for token, count in sorted(token_totals.iteritems(), key=itemgetter(1), reverse=True):
         token_counts_file.write(unicode(token +","+ str(count) +"\r\n"))
     token_counts_file.close()
@@ -72,7 +72,8 @@ def main():
         overall_token_counts = defaultdict(int)
         token_user_counts = defaultdict(int)
         user_totals = defaultdict(int)
-        user_token_file = codecs.open(USER_TOKENS_FILE, encoding="utf-8",mode= "w")
+        debug('opening ' + ALL_USER_TOKEN_COUNTS_FILE)
+        user_token_file = codecs.open(ALL_USER_TOKEN_COUNTS_FILE, encoding="utf-8",mode= "w")
         last_username = None
         for (username, token) in tokenizer.get_tokens_from_all_files():
             if ((last_username) and last_username!=username):
@@ -85,6 +86,7 @@ def main():
 
         #don't forget to write counts for that last user
         write_token_counts(user_token_file, last_username, token_user_counts)
+        debug("closing " + ALL_USER_TOKEN_COUNTS_FILE)
         user_token_file.close()
 
         write_token_totals(overall_token_counts)
